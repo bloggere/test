c1baca181b0985bb8968d2a6a5438659a757b45e
Artur <82472015+benyaar@users.noreply.github.com>
2023-01-26 20:31:06 +0200

    'artur'

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express()
 const port = 5000
 
 
-app.get('/', (req, res) => {
-  res.send('Hello Artur!')
+app.post('/', (req, res) => {
+  res.send('Hello Andrei!')
 })
 
 app.get('/', (req, res) => {
