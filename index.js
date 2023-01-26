e5d0fca1fbfc664a775a1e8d3fc994d90035ce5c
bloggere <apitestblogger@gmail.com>
2023-01-26 20:50:11 +0200

    gfyer

--- index.js
+++ index.js
@@ -25,3 +25,11 @@ app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
   
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+  
